c95c92c8557350616c938df5d9de4a21387eecdd
Dustin Getz <dustin.getz@gmail.com>
2012-01-02 01:00:01 -0500

    async controller started

---
+++ contuations/async-controller.rb
@@ -0,0 +1,52 @@
+require 'io/wait'
+
+$delay = 5
+
+
+def query_blocking query
+  p = IO.popen("sleep #{$delay}; echo #{query}")
+  p.readlines
+end
+
+def main_blocking
+  puts query_blocking "foo"
+  puts query_blocking "bar"
+end
+
+
+$pipes = []
+def query_async query, callback
+  pipe = IO.popen("sleep #{$delay}; echo #{query}")
+  pipes.push({:pipe => pipe, :callback => callback})
+end
+
+def poll_pipes
+  $pipes.reject! do |item|
+    pipe = item[:pipe]
+    if pipe.ready? && pipe.eof?  #??
+      data = pipe.read #??
+      callback = item[:callback]
+      $pipes.delete_at(i)
+      callback.call data
+      return true #prune this item
+    end
+    false #keep this item
+  end
+end
+
+def poll_keyboard
+  if STDIN.ready?
+    char = STDIN.getc
+    puts "got char `#{char}`"
+    #query_async("hello world", { |item| puts item })
+  end
+end
+
+def reactor
+  system("stty raw -echo")
+  while true
+    #poll_pipes
+    poll_keyboard
+  end
+end
+
