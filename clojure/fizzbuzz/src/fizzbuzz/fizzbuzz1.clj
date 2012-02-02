2208e9a70727b82b625e46446ba1703050b2c92f
Dustin Getz <dustin.getz@gmail.com>
2012-02-02 16:44:09 -0500

    fizzbuzz simpler clojure version

---
+++ clojure/fizzbuzz/src/fizzbuzz/fizzbuzz1.clj
@@ -0,0 +1,13 @@
+(ns fizzbuzz.core
+  (:use [clojure.contrib.string :only (join)]))
+
+(defn multiple-of? [a b]
+  (zero? (mod a b)))
+
+(defn fizzbuzz [n]
+  (cond (multiple-of? n 15) "fizzbuzz"
+        (multiple-of? n 3) "fizz"
+        (multiple-of? n 5) "buzz"
+        :else (str n)))
+
+(join ", " (map fizzbuzz (range 1 20)))
