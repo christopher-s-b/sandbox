b22ee70ece7566a8059288aa1a031db48d0ae0a1
Dustin Getz <dgetz@wingspan.com>
2012-02-11 18:21:20 -0500

    embedly3 extract method

--- etc/embedly3.py
+++ etc/embedly3.py
@@ -2,12 +2,11 @@
 add = lambda x,y: x+y
 countWords = lambda occs: reduce(add, occs, 0)
 
-occs = [2520/(n+1) for n in range(5)] #900
+occs = [2520/(n) for n in range(1,6)] #900
 num_words = countWords(occs) 
 
 for guess in range(1,101):
-    count = reduce(add, occs[:guess], 0)
-    if count >= num_words/2: break
+    if countWords(occs[:guess]) >= num_words/2: break
 
 print occs
 print num_words, num_words/2 
