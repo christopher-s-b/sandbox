907232c28e97094ae148f4bdd429f36a724a0acf
Dustin Getz <dgetz@wingspan.com>
2012-02-11 18:05:53 -0500

    embedly3

--- etc/embedly3.py
+++ etc/embedly3.py
@@ -1,8 +1,16 @@
-occs = [2520/(n+1) for n in range(900)]
-num_words = reduce(lambda x,y: y+x, occs, 0)
+
+add = lambda x,y: x+y
+countWords = lambda occs: reduce(add, occs, 0)
+
+occs = [2520/(n+1) for n in range(5)] #900
+num_words = countWords(occs) 
 
 for guess in range(1,101):
-    count = reduce(lambda x,y: y+x, occs[:guess], 0)
+    count = reduce(add, occs[:guess], 0)
     if count >= num_words/2: break
 
-print guess, count # 21 9184
+print occs
+print num_words, num_words/2 
+print guess, count
+print occs[:guess]
+
