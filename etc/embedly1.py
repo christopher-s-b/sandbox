1a5411e5d08b877b48c51c18531535b4ae31b968
Dustin Getz <dgetz@wingspan.com>
2012-02-11 23:26:59 -0500

    embedly1 sexier

--- etc/embedly1.py
+++ etc/embedly1.py
@@ -1,8 +1,23 @@
-import math
+# n! means n * (n - 1) * ... * 3 * 2 * 1
+# For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800
+# Let R(n) equal the sum of the digits in the number n!
+# For example, R(10) is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+# Find the lowest value for n where R(n) is 8001.
 
-sum = lambda n: reduce(lambda a,b:a+b, [ int(c) for c in str(math.factorial(n)) ], 0)
+from operator import mul, add
+
+oneTo = lambda n: range(1, n+1)
+assert [1,2,3,4,5] == oneTo(5)
+
+fac = lambda n: reduce(mul, oneTo(n), 1)
+assert 3628800 == fac(10)
+
+digits = lambda n: [int(c) for c in str(n)]
+assert [1, 2, 3] == digits("123")
+
+R = lambda n: sum(digits(fac(n)))
+assert 27 == R(10)
+
 r = range(1,800)
-sums = map(sum, r)
-print filter(lambda n: n[0]==8001, zip(sums,r))
-
+print filter(lambda n: n[0]==8001, zip(map(R, r), r))
 
