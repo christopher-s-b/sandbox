184ff4b2fc2d7b809f74a09a8fbf9295c8f1e354
Dustin Getz <dustin.getz@gmail.com>
2012-02-12 21:03:13 -0500

    greplin2 - last_filter and take

--- etc/greplin2.py
+++ etc/greplin2.py
@@ -1,3 +1,6 @@
+# compute the smallest prime fibonacci, X, greater than 227,000
+# compute sum of prime divisors of X+1
+
 from math import sqrt
 
 def fib_gen():
@@ -6,16 +9,18 @@ def fib_gen():
         yield a
         a, b = b, a + b
 
-def is_prime(n):
-    # imperative version avoids work, is this
-    # possible with lazy seqs?
-    return len(divisors(n)) == 0
+g=fib_gen()
+assert [0, 1, 1, 2, 3, 5, 8] == [g.next() for x in range(7)]
+
 
 def divisors(n):
     possible_divisors = range(2, int(sqrt(n))+1)
     remainders = map(lambda x: n % x, possible_divisors)
     tuples = filter(lambda x: x[1]==0, zip(possible_divisors, remainders))
-    
+
+    # fascinating - branch complexity necessary because dynamic
+    # language can't know that the empty-list is actually an empty-list
+    # of tuples - information was lost
     if len(tuples) == 0:
         return []
     else:
@@ -24,17 +29,33 @@ def divisors(n):
 
 assert divisors(24) == [2, 3, 4]
 
-def take_filter(pred, gen, N):
+def is_prime(n):
+    # imperative version avoids work, is this
+    # possible with lazy seqs?
+    return len(divisors(n)) == 0
+
+assert is_prime(37)
+assert not is_prime(38)
+
+
+def lazy_filter(pred, gen):
+    while True:
+        x = gen.next()
+        if pred(x):
+            yield x
+
+assert 13 == lazy_filter(lambda x: x > 10, fib_gen()).next()
+
+def take(gen, N):
     "take N elements from an infinite generatpr, filtered by pred"
     found = []
     while len(found) < N:
-        x = gen.next()
-        if pred(x):
-            found.append(x)
+        found.append(gen.next())
     return found
 
-assert 13 == take_filter(lambda x: x > 10 and is_prime(x), fib_gen(), 1)[0]
+assert [13, 89, 233] == take(lazy_filter(lambda x: x > 10 and is_prime(x), fib_gen()), 3)
+
 
 pred = lambda x: x > 227000 and is_prime(x)
-X = take_filter(pred, fib_gen(), 1)[0]
+X = take(lazy_filter(pred, fib_gen()), 1)[0]
 print sum(filter(is_prime, divisors(X+1)))
