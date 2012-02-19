523b6fef7ae717ec17630223fe765e9eaccbc82c
Dustin Getz <dgetz@wingspan.com>
2012-02-19 13:38:55 -0500

    embedly3 - more idiomatic python

--- etc/embedly3.py
+++ etc/embedly3.py
@@ -1,22 +1,12 @@
 
-add = lambda x,y: x+y
-countWords = lambda occs: reduce(add, occs, 0)
+occs = [2520/(n+1) for n in range(900)]
+assert 900 == len(occs)
+assert [2520, 1260, 840, 630, 504] == occs[:5]
 
-occs = [2520/(n+1) for n in range(5)] #900
-num_words = countWords(occs) 
-
+num_words = sum(occs)
 
 for guess in range(100):
-    count = countWords(occs[:guess])
+    count = sum(occs[:guess])
     if count >= num_words/2: break
 
-print occs
-print num_words, num_words/2 
-print guess, count
-print occs[:guess]
-
-# output:
-# [2520, 1260, 840, 630, 504]
-# 5754 2877
-# 2 3780
-# [2520, 1260]
+assert 21 == len(occs[:guess])
