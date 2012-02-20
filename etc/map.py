e05bfc518c404398338ad4e90b28b02fa00ca831
Dustin Getz <dgetz@wingspan.com>
2012-02-20 14:01:26 -0500

    map.py - reduce impl

--- etc/map.py
+++ etc/map.py
@@ -1,3 +1,4 @@
+
 def first(seq): return seq[0]
 def rest(seq): return seq[1:]
 
@@ -6,16 +7,17 @@ assert [2, 3] == rest([1, 2, 3])
 
 
 def map2(fn, seq):
+    "imperative map"
     return [fn(item) for item in seq]
 
 assert [2,4,6] == map2(lambda x: 2*x, [1,2,3])
 
 
-def map3(fn, seq, mapped=[]):
-    if len(seq) == 0: return mapped
-    else: return map3(fn, seq[1:], mapped + [fn(seq[0])])
+def reduce2(fn, seq, acc):
+    if seq == []: return acc
+    else: return reduce2(fn, rest(seq), fn(acc, first(seq)))
 
-assert [2,4,6] == map3(lambda x: 2*x, [1,2,3])
+assert 6 == reduce2(add, [1,2,3], 0)
 
 
 def cat(list, el):
@@ -23,9 +25,8 @@ def cat(list, el):
 
 assert [1,2,3] == cat([1,2], 3)
 
-def map4(fn, seq):
-    return reduce(
-        lambda list, el: cat(list, fn(el)),
-        seq, [])
+def map3(fn, seq):
+    accum = lambda list, el: cat(list, fn(el))
+    return reduce2(accum, seq, [])
 
-assert [2,4,6] == map4(lambda x: 2*x, [1,2,3])
+assert [2,4,6] == map3(lambda x: 2*x, [1,2,3])
