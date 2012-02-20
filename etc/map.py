7169d9a26002b83df5697855df31f58a7dd140c5
Dustin Getz <dgetz@wingspan.com>
2012-02-20 11:48:32 -0500

    map.py: impl using reduce

--- etc/map.py
+++ etc/map.py
@@ -6,10 +6,7 @@ assert [2, 3] == rest([1, 2, 3])
 
 
 def map2(fn, seq):
-    r = []
-    for item in seq:
-        r.append(fn(item))
-    return r
+    return [fn(item) for item in seq]
 
 assert [2,4,6] == map2(lambda x: 2*x, [1,2,3])
 
@@ -18,5 +15,17 @@ def map3(fn, seq, mapped=[]):
     if len(seq) == 0: return mapped
     else: return map3(fn, seq[1:], mapped + [fn(seq[0])])
 
-
 assert [2,4,6] == map3(lambda x: 2*x, [1,2,3])
+
+
+def cat(list, el):
+    return list + [el]
+
+assert [1,2,3] == cat([1,2], 3)
+
+def map4(fn, seq):
+    return reduce(
+        lambda list, el: cat(list, fn(el)),
+        seq, [])
+
+assert [2,4,6] == map4(lambda x: 2*x, [1,2,3])
