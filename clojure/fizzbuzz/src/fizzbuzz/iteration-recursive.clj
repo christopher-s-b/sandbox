f5292f338e4616ae9d6056aa4e7cbc51188af5bd
Dustin Getz <dustin.getz@gmail.com>
2012-02-26 15:14:20 -0500

    SICP: length, idiomatic  to turn recursion w/o tailcalls into iterations

--- clojure/fizzbuzz/src/fizzbuzz/iteration-recursive.clj
+++ clojure/fizzbuzz/src/fizzbuzz/iteration-recursive.clj
@@ -1,5 +1,34 @@
 (ns sicp.ch2)
 
+(defn length [items]
+  (if (empty? items)
+    0
+    (inc (length (rest items))))) ; not tail recursive
+(assert (= 3 (length [1 2 3])))
+
+;; how can we make this tail recursive? use the iter pattern
+
+(defn length-2 [items]
+  (defn iter [count items]
+    (if (empty? items)
+      count
+      (recur (inc count) (rest items)))) ; tail recursive
+  (iter 0 items))
+(assert (= 3 (length-2 [1 2 3])))
+
+;; clojure provides a special form to make iteration more idiomatic
+
+(defn length-3 [items]
+  (loop [count 0 items items]
+    (if (empty? items)
+      count
+      (recur (inc count) (rest items))))) ; tail recursive
+(assert (= 3 (length-3 [1 2 3])))
+
+;; so, loop is a construct for helping turn algorithms which aren't naturally
+;; tail recursive into iterative items with tail-recursion
+
+
 (defn square [x] (* x x))
 
 (defn square-list [items]
@@ -25,7 +54,7 @@
 (assert (= [1 4 9 16] (square-list-2 [1 2 3 4])))
 
 (defn square-list-3 [items]
-  (defn iter [things answer]
+  (defn iter [things answer] ; would be more idiomatic to use `loop`
     (if (empty? things)
       answer
       (recur (rest things) ; tail call
