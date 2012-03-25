3fedebd789ae4da7c7dfad921853cb0928390563
Dustin Getz <dgetz@wingspan.com>
2012-03-25 18:40:13 -0400

    clarified question in position.clj

--- clojure/joy/src/joy/position.clj
+++ clojure/joy/src/joy/position.clj
@@ -1,11 +1,13 @@
-(ns joy.ch5)
+(ns joy.ch5) ;; Fogus & Houser, Joy of Clojure
 
+;; provided by book
 (defn index [coll]
   (cond
     (map? coll) (seq coll)
     (set? coll) (map vector coll coll)
     :else (map vector (iterate inc 0) coll)))
 
+;; book gives this as idiomatic code
 (defn pos [needle coll]
   (for [[i val] (index coll)
         :when (= needle val)] i))
@@ -13,6 +15,7 @@
 (let [coll [:0 :1 :2 :3 :4 :5 :4]]
   (pos :4 coll))
 
+;; how can i make this better, and why provide :when, given filter?
 (defn pos2 [needle coll]
   (let [pairs (filter (fn [[i val]] (= val needle))
                       (index coll))]
