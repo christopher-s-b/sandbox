c419808c3a6ae39b2c288302924d695d5119a32a
Dustin Getz <dgetz@wingspan.com>
2012-04-03 20:00:41 -0400

    monad - maybe monad

--- etc/monads.py
+++ etc/monads.py
@@ -7,26 +7,24 @@ def bind(mv, mf):
 def result(mv):
     return mv
 
-def yes(x): return x
 
-def no(x): return None
 
 
 def userid_from_name(person_name):
-    if person_name == "Irek": return yes(1)
-    elif person_name == "John": return yes(2)
-    elif person_name == "Alex": return yes(3)
-    elif person_name == "Nick": return yes(1)
-    else: return no("No account associated with name %s" % person_name)
+    if person_name == "Irek": return 1
+    elif person_name == "John": return 2
+    elif person_name == "Alex": return 3
+    elif person_name == "Nick": return 1
+    else: return None
 
 def balance_from_userid(userid):
-    if userid == 1: return yes(1000000)
-    elif userid == 2: return yes(75000)
-    else: return no("no balance associated with account #%s" % userid)
+    if userid == 1: return 1000000
+    elif userid == 2: return 75000
+    else: return None
 
 def balance_qualifies_for_loan(balance):
-    if balance > 200000: return yes(balance)
-    else: return no("insufficient funds for loan, current balance is %s" % balance)
+    if balance > 200000: return balance
+    else: return None
 
 def name_qualifies_for_loan(person_name):
 
@@ -37,5 +35,8 @@ def name_qualifies_for_loan(person_name):
     return result(mLoan)
 
 
-for person_name in ["Irek", "John", "Alex", "Nick", "Dustin"]:
-    print "%s: %s" % (person_name, name_qualifies_for_loan(person_name))
+
+
+for person_name in ["Irek", "John", "Alex", "Nick", "Fake"]:
+    qualified = name_qualifies_for_loan(person_name)
+    print "%s: %s" % (person_name, qualified)
