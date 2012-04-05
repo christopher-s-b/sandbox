31330df679a75d49b660664d724162bba3e9d500
Dustin Getz <dgetz@wingspan.com>
2012-04-04 20:41:32 -0400

    python monads

--- etc/monads.py
+++ etc/monads.py
@@ -51,19 +51,17 @@ def bind(mv, mf):
     if (mv): return mf(mv)
     else: return None
 
-def lift(value):
+def unit(value):
     "takes a simple value and turns it into a monadic value (for use with bind)"
     return value
 
 def name_qualifies_for_loan(person_name):
-    "note pattern of lift-bind-bind-bind, we can abstract further with macros"
-    mName =    lift(person_name)
+    "note pattern of unit-bind-bind-bind, we can abstract further with macros"
+    mName =    unit(person_name)
     mUserid =  bind(mName, userid_from_name)
     mBalance = bind(mUserid, balance_from_userid)
     mLoan =    bind(mBalance, balance_qualifies_for_loan)
-
     return mLoan
-
 
 for person_name in ["Irek", "John", "Alex", "Nick", "Fake"]:
     qualified = name_qualifies_for_loan(person_name)
@@ -73,42 +71,57 @@ print
 
 # add error codes to the plumbing
 
-
-
-# helpers for returning error codes
-def success(x): return (True, x)
-def fail(x): return (False, x)
-
 def userid_from_name(person_name):
-    if person_name == "Irek": return success(1)
-    elif person_name == "John": return success(2)
-    elif person_name == "Alex": return success(3)
-    elif person_name == "Nick": return success(1)
-    else: return fail("No account associated with name '%s'" % person_name)
+    if person_name == "Irek": return 1, None
+    elif person_name == "John": return 2, None
+    elif person_name == "Alex": return 3, None
+    elif person_name == "Nick": return 1, None
+    else: return None, "No account associated with name '%s'" % person_name
 
 def balance_from_userid(userid):
-    if userid == 1: return success(1000000)
-    elif userid == 2: return success(75000)
-    else: return fail("No balance associated with account #%s" % userid)
+    if userid == 1: return 1000000, None
+    elif userid == 2: return 75000, None
+    else: return None, "No balance associated with account #%s" % userid
 
 def balance_qualifies_for_loan(balance):
-    if balance > 200000: return success(balance)
-    else: return fail("Insufficient funds for loan, current balance is %s" % balance)
+    if balance > 200000: return balance, None
+    else: return None, "Insufficient funds for loan, current balance is %s" % balance
 
 
 # bind knows how to unwrap the return value and pass it to
 # the next function
 def bind(mv, mf):
-    succeeded = mv[0]
-    value = mv[1]
+    value = mv[0]
+    error = mv[1]
+    if not error:
+        return mf(value)
+    else:
+        return mv
 
-    if (succeeded): return mf(value)
-    else: return mv
+def unit(value):
+    return value, None
 
-def lift(val): return success(val)
-
-#same monadic code as before
+def name_qualifies_for_loan(person_name):
+    mName =    unit(person_name)
+    mUserid =  bind(mName, userid_from_name)
+    mBalance = bind(mUserid, balance_from_userid)
+    mLoan =    bind(mBalance, balance_qualifies_for_loan)
+    return mLoan
 
 for person_name in ["Irek", "John", "Alex", "Nick", "Fake"]:
     qualified = name_qualifies_for_loan(person_name)
     print "%s: %s" % (person_name, qualified)
+print
+
+
+
+def mmap(mf, mvs):
+    return map(lambda x: bind(x, mf), mvs)
+
+names = ["Irek", "John", "Alex", "Nick", "Fake"]
+mNames = map(unit, names)
+mDecisions = mmap(name_qualifies_for_loan, mNames)
+
+
+for name, mDecision in zip(names, mDecisions):
+    print "%s: %s" % (name, mDecision)
