f07e055b75509f55cb19f9009110ebf815649901
Dustin Getz <dustin.getz@gmail.com>
2012-08-06 14:50:31 -0400

    interpreter

--- etc/lexicalscope.py
+++ etc/lexicalscope.py
@@ -1,57 +1,53 @@
-
+import operator as op
 
 def head(seq): return seq[0]
 def tail(seq): return seq[1:]
 
+def eval1(form):
+    if form[0] == Literal:
+        return form
+    elif form[0] == Symbol:
+        if form[1] == "+": return func(op.add)
+        elif form[1] == "x": return lit(1)
+        else: assert False, "unsupported symbol `%s`" % form[1]
+    elif form[0] == Sexpr:
+        return evalS(form[1])
+    else: assert False, "unsupported form type"
 
-class Expr:
-    def __init__(self, *args):
-        self.args = args
-    def eval(self, env):
-        """recursively evaluate the expr until there are no terms left"""
-        args = self.args
-        term, args = head(args), tail(args)
-
-        if isinstance(term, Lambda):
-            rhs, args = head(args), tail(args)
-            return term.apply(env, rhs)
-        elif isinstance(term, Symbol):
-            return val.eval(
-
-        #if f == "+":
-        #    lhs, args = head(args), tail(args)
-        #    return rhs.eval(env) + lhs.eval(env)
-        #return None
-
-    def reduce(self, env):
-        if isinstance(self, Literal): return self
-        expr = self.eval(env)
-        self.reduce(expr, env)
-
-class Lambda:
-    def __init__(self, *args):
-        pass
-    def apply(self, env):
+def evalS(sexpr):
+    """recursively evaluate the form until there are no terms left"""
+    form, forms = head(sexpr), tail(sexpr)
+    fn = eval1(form)
+    assert fn[0] == Func, "form in sexpr head position must resolve to a func"
+    return apply(fn, forms)
 
 
-class Symbol(Expr):
-    def eval(self, env):
-        sym = head(self.args)
-        return env[sym]
+def apply(fn, args):
+    args = [a[1] for a in map(eval1, args)] #unpack the tuples
+    # args are allowed to be Literals or Fns (have to be native python types now)
+    return Literal, fn[1](*args)
 
-class Literal(Expr):
-    def eval(self, env):
-        lit = head(self.args)
-        return lit
+class Symbol: pass
+class Literal: pass
+class Func: pass
+class Sexpr: pass
 
+def sym(tok): return (Symbol, tok)
+def lit(tok): return (Literal, tok)
+def sexpr(tok): return (Sexpr, tok)
+def func(tok): return (Func, tok)
 
+assert eval1(lit(1)) == (Literal, 1)
+assert eval1(sym("x")) == (Literal, 1)
+assert eval1(sym("+")) == (Func, op.add)
 
+assert eval1(sexpr([sym("+"), sym("x"), lit(3)])) == (Literal, 4)
+assert evalS([sym("+"), sym("x"), lit(3)]) == (Literal, 4)
 
-env = {"x": 3, "addOne": Lambda(
-assert Expr("addOne", Symbol("x")).eval({"x": 3}) == 4
-
-
-
+assert eval1(
+         sexpr([sym("+"), sym("x"),
+           sexpr([sym("+"), lit(42), lit(1)])
+           ])) == (Literal, 44)
 
 
 # "(\x -> + x 1) 3"
