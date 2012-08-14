868d61d2000d30f8ad1bc731daa3a717df414b68
Dustin Getz <dustin.getz@gmail.com>
2012-08-14 17:00:59 -0400

    lispy: tests pass with error-m threaded through

--- etc/lis.py
+++ etc/lis.py
@@ -5,7 +5,10 @@
 ################ Symbol, Env classes
 
 from __future__ import division
+import math, operator as op
+
 from monads import error_m, err
+
 
 interp_m = error_m # cont_t state_t error_t identity_m
 
@@ -26,35 +29,42 @@ class Env(dict):
         self.outer = outer
     def find(self, var):
         "Find the innermost Env where var appears."
+        assert (var in self) or self.outer, "unbound var: %s"%var
         return self if var in self else self.outer.find(var)
+
+py_primitive_fns = {
+    '+':op.add,
+    '-':op.sub,
+    '*':op.mul,
+    '/':op.div,
+    'not':op.not_,
+    '>':op.gt,
+    '<':op.lt,
+    '>=':op.ge,
+    '<=':op.le,
+    '=':op.eq,
+    'equal?':op.eq,
+    'eq?':op.is_,
+    'length':len,
+    'cons':lambda x,y:[x]+y,
+    'car':lambda x:x[0],
+    'cdr':lambda x:x[1:],
+    'append':op.add,
+    'list':lambda *x:list(x),
+    'list?': lambda x:isa(x,list),
+    'null?':lambda x:x==[],
+    'symbol?':lambda x: isa(x, Symbol)
+}
+
+def lift(fn): #lift is a misnomer, its not a real monadic lift. what is it?
+    "inlining this fn seems to break python?"
+    return lambda *args: ok(fn(*args))
 
 def add_globals(env):
     "Add some Scheme standard procedures to an environment."
-    import math, operator as op
     env.update(vars(math)) # sin, sqrt, ...
-    env.update(
-     {'+':op.add,
-      '-':op.sub,
-      '*':op.mul,
-      '/':op.div,
-      'not':op.not_,
-      '>':op.gt,
-      '<':op.lt,
-      '>=':op.ge,
-      '<=':op.le,
-      '=':op.eq,
-      'equal?':op.eq,
-      'eq?':op.is_,
-      'length':len,
-      'cons':lambda x,y:[x]+y,
-      'car':lambda x:x[0],
-      'cdr':lambda x:x[1:],
-      'append':op.add,
-      'list':lambda *x:list(x),
-      'list?': lambda x:isa(x,list),
-      'null?':lambda x:x==[],
-      'symbol?':lambda x: isa(x, Symbol)
-     })
+    for sym, fn in py_primitive_fns.iteritems():
+        env[sym] = lift(fn)
     return env
 
 global_env = add_globals(Env())
@@ -66,50 +76,67 @@ isa = isinstance
 def eval(x, env=global_env):
     "Evaluate an expression in an environment."
     if isa(x, Symbol):             # variable reference
+        assert env
         return ok(env.find(x)[x])
+
     elif not isa(x, list):         # constant literal
         return ok(x)
+
     elif x[0] == 'quote':          # (quote exp)
         (_, exp) = x
         return ok(exp)
+
     elif x[0] == 'if':             # (if test conseq alt)
         (_, test, conseq, alt) = x
         def _doif(val):
             return (conseq if val else alt)
-        branch = fmap(eval(test, env), _doif)
-        return eval(branch)
+        mbranch = fmap(_doif, eval(test, env))
+        return bind(mbranch, lambda branch: eval(branch, env))
+
     elif x[0] == 'set!':           # (set! var exp)
         (_, var, exp) = x
         def _doset(val):
             #assert env.find(var) -- this is a RuntimeError
+            assert env
             env.find(var)[var] = val
             return None
-        return fmap(eval(exp, env), _doset)
+        return fmap(_doset, eval(exp, env))
+
     elif x[0] == 'define':         # (define var exp)
         (_, var, exp) = x
         def _dodefine(val):
             env[var] = val
             return None
-        return fmap(eval(exp, env), _dodefine)
+        return fmap(_dodefine, eval(exp, env))
 
     elif x[0] == 'lambda':         # (lambda (var*) exp)
         (_, vars, exp) = x
-        return ok(lambda *args: eval(exp, Env(vars, args, env))) # ?
+        return ok(lambda *args: eval(exp, Env(vars, args, env)))
 
     elif x[0] == 'begin':          # (begin exp*)
         exprs = x[1:]
-        mvs = mmap(lambda exp: eval(exp, env),
-                   exprs)
-        return mvs[-1]
-        #for exp in x[1:]:
-        #    val = eval(exp, env)
-        #return val
+
+        # this code is a recursive monad comprehension, it composes the plumbing
+        # such that errors short circuit etc, but discards the passed in value
+        # and just evaluates the next expression. the return value is monadic -
+        # the last expression evaluated.
+        #
+        # version if there wasn't a monad:
+        #
+        #   for exp in x[1:]:
+        #     val = eval(exp, env)
+        #   return val
+        #
+        def _dobegin(exp, exprs): # -> mv
+            if not exprs: return eval(exp, env)
+            return bind(eval(exp, env), lambda _: _dobegin(exprs[0], exprs[1:]))
+        return _dobegin(exprs[0], exprs[1:])
+
     else:                          # (proc exp*)
         mvs = map(lambda exp: eval(exp, env), x)
         mproc, margs = mvs.pop(0), mvs
-        print "mproc: %s, margs: %s"%(mproc, margs)
         args = seq(margs)
-        return fmap(lambda proc: proc(*args), mproc)
+        return bind(mproc, lambda proc: proc(*args))
 
 ################ parse, read, and user interaction
 
