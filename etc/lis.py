ac57ea64b435dc990a0517c14fab245d09b1033e
Dustin Getz <dustin.getz@gmail.com>
2012-08-14 15:59:50 -0400

    lispy: format code of default env to be one thing per line

--- etc/lis.py
+++ etc/lis.py
@@ -33,12 +33,28 @@ def add_globals(env):
     import math, operator as op
     env.update(vars(math)) # sin, sqrt, ...
     env.update(
-     {'+':op.add, '-':op.sub, '*':op.mul, '/':op.div, 'not':op.not_,
-      '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq,
-      'equal?':op.eq, 'eq?':op.is_, 'length':len, 'cons':lambda x,y:[x]+y,
-      'car':lambda x:x[0],'cdr':lambda x:x[1:], 'append':op.add,
-      'list':lambda *x:list(x), 'list?': lambda x:isa(x,list),
-      'null?':lambda x:x==[], 'symbol?':lambda x: isa(x, Symbol)})
+     {'+':op.add,
+      '-':op.sub,
+      '*':op.mul,
+      '/':op.div,
+      'not':op.not_,
+      '>':op.gt,
+      '<':op.lt,
+      '>=':op.ge,
+      '<=':op.le,
+      '=':op.eq,
+      'equal?':op.eq,
+      'eq?':op.is_,
+      'length':len,
+      'cons':lambda x,y:[x]+y,
+      'car':lambda x:x[0],
+      'cdr':lambda x:x[1:],
+      'append':op.add,
+      'list':lambda *x:list(x),
+      'list?': lambda x:isa(x,list),
+      'null?':lambda x:x==[],
+      'symbol?':lambda x: isa(x, Symbol)
+     })
     return env
 
 global_env = add_globals(Env())
