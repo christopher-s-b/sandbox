b0996666eb4af0f58b10faf4cbe7b3b6695c7402
Dustin Getz <dustin.getz@gmail.com>
2012-08-17 17:08:02 -0400

    test for reader monad

--- etc/monads.py
+++ etc/monads.py
@@ -232,45 +232,21 @@ class _Reader_m(Monad):
         return _
 
 reader_m = _Reader_m()
+def ask(env): return env
 
-
-from collections import namedtuple
 def _test_reader_m():
+    bind = reader_m.bind
+    unit = reader_m.unit
+    fmap = reader_m.fmap
 
-    class Servlet:
-        def __init__(log, settings):
-            self.log = log
-            self.settings = settings
+    def getA(env): return env["a"]
 
-        def post(request, db): pass
-        def get(request, db): pass
+    def f1(): return fmap(ask, lambda env: env["a"])
+    def f2(): return fmap(getA, lambda a: a)
 
-    class PageServlet(Servlet): pass
-    class DatastoreServlet(Servlet): pass
-
-
-
-    Env = namedtuple('Env', ['hostname', 'port', 'outfile'])
-    # getters describe how to read a value from an environment,
-    # but do not have access to the environment itself
-    hostname = lambda env: env.hostname
-    port = lambda env: env.port
-    outfile = lambda env: env.outfile
-
-    servlets = []
-
-    def bootstrap():
-        """no dependencies passed as parameters. could have very long list of
-        dependencies"""
-        r = reader_m.bind( hostname, lambda h:
-            reader_m.bind( port,     lambda p:
-            reader_m.bind( outfile,  lambda o:
-            reader_m.unit( doSomething(h,p,o) ))))
-        return r
-
-    env = Env("localhost", 80, "/etc/passwd")
-    assert bootstrap()(env) == ["/etc/passwd", 80, "localhost"]
-
+    env = {"a": "42"}
+    assert f1()(env) == "42"
+    assert f2()(env) == "42"
 
 
 
@@ -280,8 +256,8 @@ def _run_tests():
     _test_error_m()
     _test_list_m()
     _test_writer_m()
-    #_test_reader_m()
+    _test_reader_m()
     print("all tests passed")
 
 
-#if __name__=="__main__": _run_tests()
+if __name__=="__main__": _run_tests()
