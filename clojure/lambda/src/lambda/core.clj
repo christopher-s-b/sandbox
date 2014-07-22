4f10278eb708336c29aab0f6ac7d318d7f568a6b
Dustin Getz <dustin.getz@gmail.com>
2014-07-22 17:20:53 -0500

    lambda stuff wip

--- clojure/lambda/src/lambda/core.clj
+++ clojure/lambda/src/lambda/core.clj
@@ -1,35 +1,31 @@
 (ns lambda.core)
 
-
 (def env { 'a 1
            'b 2
-           'add (fn [x y] (+ x y))
+           '+ +
            })
 
-;; reduce a list of terms into the value
-;; take the first term
-;; if it is 
+
+(defn meval
+  [term] (cond
+          (contains? env term) (env term)
+          :else term)
+
+  [term f & args]
+    (cond
+     (= term 'app) (apply (meval f) (map meval args)))
+    
+)
+
+(comment
+  (def expr ['app '+ 'a 'b])  
+  (apply meval expr)
+
+  (meval ['a])
+  (meval ['app '+ 'a 1])
+
+'app '+ 'a '1
+'app '+ 'a '('+ 'a 'a)
 
 
-(defn meval [expr]
-  (cond
-   (= (first expr) 'app) ((env (nth expr 1)) (env (nth expr 2)) (env (nth expr 3)))
-   ))
-
-
-;; data Term
-;;   = Var String
-;;   | App Term Term
-;;   | Lam String Term
-
-
-;; App (Lam x b) a
-;;(def expr ['app '('lam 'x 'b) 'a])
-(def expr ['app 'add 'a 'b])
-
-
-
-
-
-;; App (Lam x (Lam y (Var x))) (Var y)
-;; 'app ('lam "x" ('lam "y" ('var "x"))) ('var "y")
+  )
