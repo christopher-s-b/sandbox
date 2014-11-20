85922596eb453d04e95132f8819967e6785c6637
Dustin Getz <dustin.getz@gmail.com>
2014-11-20 16:47:30 -0500

    Add command line options support to main wrapper program

--- outpace-test/src/Main.hs
+++ outpace-test/src/Main.hs
@@ -1,13 +1,39 @@
 module Main where
 
+import System.Environment
 import System.IO
 import BankOcr
+import Options.Applicative
 
-main :: IO ()
-main = do
-  hIn <- openFile "sample.txt" ReadMode
-  hOut <- openFile "results.txt" WriteMode
+data BankOcrOpts = BankOcrOpts
+  { infile :: String
+  , outfile :: String }
+
+sample :: Parser BankOcrOpts
+sample = BankOcrOpts
+  <$> strOption
+      ( long "infile"
+     <> metavar "FILE"
+     <> help "input file containing raw Bank OCR scanner data" )
+  <*> strOption
+      ( long "outfile"
+     <> metavar "FILE"
+     <> help "output file for results" )
+
+doit :: BankOcrOpts -> IO ()
+doit (BankOcrOpts ifile ofile) = do
+  hIn <- openFile ifile ReadMode
+  hOut <- openFile ofile WriteMode
   contents <- hGetContents hIn
   hPutStrLn hOut $ parseBankOcrSoup contents
   hClose hIn
   hClose hOut
+
+  
+main :: IO ()
+main = execParser opts >>= doit
+  where
+    opts = info (helper <*> sample)
+      ( fullDesc
+     <> progDesc "Parse and interpret Bank OCR scanner machine raw output"
+     <> header "bankocrmain - a utility for parsing Bank OCR scanner raw output" )
